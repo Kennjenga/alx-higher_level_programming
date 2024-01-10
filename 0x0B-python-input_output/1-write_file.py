6b4f98a1fab7dd7f9226a9499ab9cb5b517fbbd9
Kennjenga <kinyagia10@gmail.com>
2024-01-10 01:41:10 -0800

    script that adds all arguments to a Python list, and then save them to a file:

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 
-"""Defines a file-writing function with error handling."""
+"""Defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF-8 text file.
+    """Write a string to a UTF8 text file.
 
     Args:
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
     Returns:
-        None
+        The number of characters written.
     """
-
-    try:
-        with open(filename, "w", encoding="utf-8") as f:
-            f.write(text)
-    except (IOError, OSError) as e:
-        print(f"Error writing to file '{filename}': {e}")
-
-
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
